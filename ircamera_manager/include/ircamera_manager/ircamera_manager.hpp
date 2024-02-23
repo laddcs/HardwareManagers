9d3590b1f32f9076886fdf02f8d17945985da84c
incarsaron <Bryce>
2024-02-23 17:24:22 -0500

    Working on ircamera_manager

--- ircamera_manager/include/ircamera_manager/ircamera_manager.hpp
+++ ircamera_manager/include/ircamera_manager/ircamera_manager.hpp
@@ -2,18 +2,19 @@
 
 #include <libirimager/IRDevice.h>
 #include <libirimager/IRImager.h>
+#include <libirimager/IRImagerClient.h>
 
 namespace ircamera_manager
 {
 
-class IRCameraManager : public rclcpp::Node
+class IRCameraManager : public rclcpp::Node, public evo::IRImagerClient
 {
     public:
         explicit IRCameraManager(const rclcpp::NodeOptions & options);
         ~IRCameraManager();
 
-    private:   
-        std::shared_ptr<evo::IRDevice> dev_;     
+    private:
+        std::shared_ptr<evo::IRDevice> dev_;
 };
 
 } // namespace ircamera_manager
