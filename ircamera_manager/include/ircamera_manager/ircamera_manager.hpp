4a5d3fcf7aca19650bae67bd5aa271ac13459a65
incarsaron <Bryce>
2024-02-26 17:07:30 -0500

    Implementing ircamera_manager

--- ircamera_manager/include/ircamera_manager/ircamera_manager.hpp
+++ ircamera_manager/include/ircamera_manager/ircamera_manager.hpp
@@ -1,8 +1,15 @@
+#include <chrono>
+#include <thread>
+
 #include <rclcpp/rclcpp.hpp>
+#include <image_transport/image_transport.hpp>
+#include <sensor_msgs/msg/image.hpp>
 
 #include <libirimager/IRDevice.h>
 #include <libirimager/IRImager.h>
 #include <libirimager/IRImagerClient.h>
+#include <libirimager/IRLogger.h>
+#include <libirimager/IRFileWriter.h>
 
 namespace ircamera_manager
 {
@@ -11,10 +18,57 @@ class IRCameraManager : public rclcpp::Node, public evo::IRImagerClient
 {
     public:
         explicit IRCameraManager(const rclcpp::NodeOptions & options);
-        ~IRCameraManager();
+        virtual ~IRCameraManager();
+
+        virtual void onRawFrame(unsigned char* data, int size) {(void)data; (void)size;};
+
+        // Called on thermal frame recieved
+        virtual void onThermalFrame(unsigned short* image, unsigned int w, unsigned int h, evo::IRFrameMetadata meta, void* arg);
+
+        virtual void onVisibleFrame(unsigned char* image, unsigned int w, unsigned int h, evo::IRFrameMetadata meta, void* arg);
+
+        virtual void onFlagStateChange(evo::EnumFlagState flagstate, void* arg);
+
+        virtual void onProcessExit(void* arg);
+
 
     private:
-        std::shared_ptr<evo::IRDevice> dev_;
+        // IR device parameters object, used to initialize device and imager
+        evo::IRDeviceParams params_;
+
+        // Header for raw data stream
+        evo::RawdataHeader header_;
+
+        // Pointer to IR device manager
+        evo::IRDevice* dev_;
+
+        // Optris image processing pipeline
+        evo::IRImager imager_;
+
+        // Writes raw serialized data stream from IRDevice to disk, this is the most efficient way to store the data (without losing precision)
+        std::shared_ptr<evo::IRFileWriter> writer_;
+
+        unsigned char* bufferRaw_;
+        char nmea_[GPSBUFFERSIZE];
+        bool run_;
+        std::thread* deviceThread_;
+
+        // Thermal image publishing
+        sensor_msgs::msg::Image thermalImage_;
+        rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr thermalPub_;
+
+        // Loop that checks for a new IR image and passes it to the image processing pipeline
+        // Executed in the deviceThread_
+        void deviceThreadRunner();
+        
+        // Checks xml parameters, initializes the IR device with given paramters
+        void initializeIRDevice();
+
+        // Initializes the IR file writer
+        void initializeFileWriter();
+
+        // Initialize node parameters
+        void initializeParameters();
 };
 
 } // namespace ircamera_manager
--- ircamera_manager/src/ircamera_manager.cpp
+++ ircamera_manager/src/ircamera_manager.cpp
@@ -2,7 +2,78 @@
 
 namespace ircamera_manager
 {
+    IRCameraManager::IRCameraManager(const rclcpp::NodeOptions & options) : Node("ircamera_manager", options)
+    {
+        initializeParameters();
 
+        initializeIRDevice();
+
+        // Initialize the device thread runner
+        run_ = true;
+        deviceThread_ = new std::thread(&IRCameraManager::deviceThreadRunner, this);
+    }
+
+    void IRCameraManager::deviceThreadRunner()
+    {
+        int serializedImages = 0;
+        int chunk = 1;
+        double timestamp;
+
+        auto durInSec = std::chrono::duration<double>(1.0/imager_.getMaxFramerate());
+        while(run_)
+        {
+            int retVal = dev_->getFrame(bufferRaw_, &timestamp);
+            if(retVal == evo::IRIMAGER_SUCCESS)
+            {
+                imager_.process(bufferRaw_);
+
+                if(writer_->canDoWriteOperations())
+                {
+                    writer_->write(timestamp, bufferRaw_, chunk, dev_->getRawBufferSize(), nmea_);
+                }
+
+                if((++serializedImages)%1000==0)
+                {
+                    chunk++;
+                }
+            }
+            if(retVal == evo::IRIMAGER_DISCONNECTED)
+            {
+                return;
+            } 
+
+            std::this_thread::sleep_for(durInSec);
+        }
+    }
+
+    void IRCameraManager::onThermalFrame(unsigned short* image, unsigned int w, unsigned int h, evo::IRFrameMetadata meta, void* arg)
+    {
+
+    }
+
+    void IRCameraManager::initializeIRDevice()
+    {
+        std::string xmlPath = this->get_parameter("device_xml_config").as_string();
+        evo::IRDeviceParamsReader::readXML(xmlPath.c_str(), params_);
+
+        RCLCPP_INFO(this->get_logger(), "Read XML Parameters!");
+
+        dev_ = evo::IRDevice::IRCreateDevice(params_);
+
+        RCLCPP_INFO(this->get_logger(), "IR Device Initialized!");
+    }
+
+    void IRCameraManager::initializeFileWriter()
+    {
+        // Initialize gps header as zeros
+        memset(nmea_, 0, GPSBUFFERSIZE*sizeof(*nmea_));
+    }
+
+    void IRCameraManager::initializeParameters()
+    {
+        this->declare_parameter("device_xml_config", rclcpp::ParameterValue(""));
+        this->declare_parameter("raw_file_path", rclcpp::ParameterValue(""));
+    }
 } // namespace ircamera_manager
 
 // Register the node as a component
