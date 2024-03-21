0280858497be66ddba5be859d6652c3342985d6e
incarsaron <Bryce>
2024-03-20 20:05:59 -0400

    Working on video pipeline

--- streamer/include/streamer/streamer.hpp
+++ streamer/include/streamer/streamer.hpp
@@ -4,7 +4,10 @@
 #include <cv_bridge/cv_bridge.h>
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/cudaimgproc.hpp>
 #include <opencv2/core/cuda.hpp>
+
+#include <cuda_runtime.h>
 
 #include <sensor_msgs/msg/image.hpp>
 
@@ -16,11 +19,19 @@ class Streamer : public rclcpp::Node
 {
     public:
         explicit Streamer(const rclcpp::NodeOptions & options);
+        ~Streamer();
 
     private:
+        void *unified_frame_in_ptr_;
+        void *unified_frame_out_ptr_;
 
         cv_bridge::CvImagePtr image_ptr_;
-        cv::cuda::GpuMat src_, dst_;
+
+        cv::Mat frame_in_;
+        cv::cuda::GpuMat d_frame_in_;
+
+        cv::Mat frame_out_;
+        cv::cuda::GpuMat d_frame_out_;
 
         // Image Subscriber
         rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr imageSub_;
--- streamer/src/streamer.cpp
+++ streamer/src/streamer.cpp
@@ -6,20 +6,45 @@ namespace streamer
 {
     Streamer::Streamer(const rclcpp::NodeOptions & options) : Node("streamer", options)
     {
+        
+        unsigned int width = 382;
+        unsigned int height = 288;
+
+        size_t in_pixels = width * height * sizeof(unsigned short);
+        size_t out_pixels = width * height * 3 * sizeof(unsigned  char);
+
+        // Allocate unified buffers
+        bool in_alloc = cudaMallocManaged(&unified_frame_in_ptr_, in_pixels);
+        bool out_alloc = cudaMallocManaged(&unified_frame_out_ptr_, out_pixels);
+
+        if (!in_alloc || !out_alloc)
+        {
+            RCLCPP_ERROR(this->get_logger(), "Unable to allocate unified buffers, fail to start stream!");
+            return;
+        }
+
+        frame_in_ = cv::Mat(height, width, CV_16U, unified_frame_in_ptr_);
+        d_frame_in_ = cv::cuda::GpuMat(height, width, CV_16U, unified_frame_in_ptr_);
+
+        frame_out_ = cv::Mat(height, width, CV_8UC3, unified_frame_out_ptr_);
+        d_frame_out_ = cv::cuda::GpuMat(height, width, CV_8UC3, unified_frame_out_ptr_);
+
         // Set QoS profile for node
         rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
         auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
 
-        std::string pipeline = "appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host=127.0.0.1 port=5000";
+        std::string pipeline = "appsrc ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host=127.0.0.1 port=5000";
         writer_ = cv::VideoWriter(pipeline, cv::CAP_GSTREAMER, 0, 30.0, cv::Size(382, 288), true);
         if (writer_.isOpened())
         {
             RCLCPP_INFO(this->get_logger(), "Stream Started");
         } else
         {
-            RCLCPP_INFO(this->get_logger(), "Failed To Start Stream");
+            RCLCPP_ERROR(this->get_logger(), "Failed To Start Stream");
+            return;
         }
 
+        // Create subscribers
         imageSub_ = this->create_subscription<sensor_msgs::msg::Image>(
             "/hardware/thermal_image",
             qos,
@@ -27,17 +52,27 @@ namespace streamer
         );
     }
 
+    Streamer::~Streamer()
+    {
+        // Free unified buffers
+        cudaFree(unified_frame_in_ptr_);
+        cudaFree(unified_frame_out_ptr_);
+
+        // Release the gstreamer pipeline
+        writer_.release();
+    }
+
     void Streamer::imageCB(const sensor_msgs::msg::Image::ConstSharedPtr & msg)
     {
         image_ptr_ = cv_bridge::toCvCopy(*msg, msg->encoding);
+        image_ptr_->image.copyTo(frame_in_);
 
-        //src_.upload(image_ptr_->image);
+        // Perform colorspace conversion using gpu
+        cv::cuda::cvtColor(d_frame_in_, d_frame_out_, cv::COLOR_GRAY2BGR, 0);
 
-        //cv::gpu::cvtColor(src_, dst_, cv::COLOR_GRAY2BGR, 0);
-
-        writer_.write(image_ptr_->image);
+        writer_.write(frame_out_);
     }
-} // namespace streamer'
+} // namespace streamer
 
 // Register the node as a component
 #include <rclcpp_components/register_node_macro.hpp>
