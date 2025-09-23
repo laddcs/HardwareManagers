fa1637c728402d90c125e663aa5104a0629ee185
incarsaron <Bryce>
2025-09-23 17:40:25 -0400

    Fixed streamer bug

--- streamer/src/streamer.cpp
+++ streamer/src/streamer.cpp
@@ -83,7 +83,7 @@ namespace streamer
         // Double the image size (Video writer throws a fit if the image is too small)
         cv::resize(frame_convert_, frame_out_, cv::Size(msg->width * 2, msg->height * 2));
 
-        writer_->write(frame_out_);
+        writer_.write(frame_out_);
     }
 } // namespace streamer
 
